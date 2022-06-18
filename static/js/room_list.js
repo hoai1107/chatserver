51cc8d821b793f64c0013b4e4bf96fec38427daa
hoai1107 <khaihoai1107@gmail.com>
2022-06-18 20:47:26 +0700

    Update room list page

---
+++ static/js/room_list.js
@@ -0,0 +1,30 @@
+window.onload = function (){
+    // TODO: Get all name of room and add to template
+    let xhr = new XMLHttpRequest();
+    let roomList = document.getElementById('list');
+    
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+
+            for(let x in data.rooms){
+                let item = createRoomLink(data.rooms[x])
+                roomList.appendChild(item)
+            }
+        }
+    };
+
+    xhr.open('GET', '/all_rooms', true);
+    xhr.send()
+}
+
+function createRoomLink(name){
+    let item = document.createElement('a');
+
+    item.innerHTML = name;
+    item.setAttribute('class', 'btn btn-secondary w-25');
+    item.setAttribute('href', `/chat?room=${name}`);
+
+    return item;
+}
+
